4eea9972e5ed09b3e941ed0dff31ac523ad5ed38
princekohli <prince.kohli@smartbuzz.net>
2013-10-08 18:26:31 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartbz::Application.config.secret_key_base = '81b202bc2fe8fcf529422226a5639ce14939e39818326bfbcd56e90eb272ae4b40e1ef2dadfa6a55b390e0976d0bd4b6b329a39d3c83be3a562dc7ce8daf3f28'
